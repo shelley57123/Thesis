cc2f6160c86486426c3a49d8056c72a7c3bb747d
shelley57123 <shelleysun1992@gmail.com>
2016-07-03 23:13:59 +0800

    [Prediction Precision] One Step Prediction

--- module/userScore.py
+++ module/userScore.py
@@ -1,6 +1,7 @@
 import numpy as np
 import ldaAdd
 from sklearn.cluster import KMeans
+from operator import itemgetter
 
 topicNum = ldaAdd.topicNum
 userClusNum = 10
@@ -59,3 +60,63 @@ def userClus(user_topic, topic_word, dic):
         print('Cluster {}: {}'.format(i, ' '.join(user_top_words)))
 
     return clus_topic
+
+
+def userTopic_minus(USER_FILE, points, doc_topic):
+
+    uall = open(USER_FILE,'r')
+    ucount = 0
+    user_topic = []
+    users = []
+    paths = []
+    t = 0
+    for line in uall.readlines():
+        ucount += 1
+        ls = line.split()
+        if len(ls)>=1 and ls[0]!='':
+            users.append(ls[0])
+            
+    for user in users:
+        user_points = points[:,1] == user
+       
+        #for all points of the user, sort by time(index 2-7)
+        tsorted = np.array(sorted(points[user_points], key=itemgetter(2,3,4,5,6,7)))
+
+        n = 0
+        person_vec = np.zeros(topicNum, dtype=float)#topic amount
+        if len(tsorted)>0:
+
+            #dates of all posts of user
+            datesToClus = np.vstack({tuple(row) for row in tsorted[:,2:5]})#remove duplicate date
+            for date in datesToClus:
+
+                #locations of the user visited in this date(boolean list)
+                same_date = tsorted[:,2:5] == date
+                sd = []
+                for d in same_date:
+                    sd.append(d.all())
+                same_date = np.array(sd, dtype=bool)
+                
+                #locations of posts in this date
+                l = len(tsorted[same_date])
+                path = tsorted[same_date]
+                diffClusIdx = -1
+                for i in range(l-1):
+                    if path[-i-2,-1] != path[-1,-1]:
+                        diffClusIdx = l-i-2
+
+                if l > 5 and diffClusIdx != -1:
+                    for val in path[:diffClusIdx+1]:
+                        person_vec += doc_topic[ val[-1] ] 
+                        n += 1
+                    paths.append([user, diffClusIdx, path])
+
+                else:
+                    for val in path:
+                        person_vec += doc_topic[ val[-1] ] 
+                        n += 1
+
+        person_vec =  person_vec / float(n)
+        user_topic.append(person_vec)
+
+    return [user_topic, users, paths]
